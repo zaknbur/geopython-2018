a7065707d00a6386cc46a6491fbd0f9ec8b9f88e
John A Stevenson <jostev@bgs.ac.uk>
2018-05-06 21:53:45 +0100

    Add better formatted points map

---
+++ iframes/points_map/data/hekla_location_2.js
@@ -0,0 +1,1 @@
+var json_hekla_location_2 = {"type":"FeatureCollection","name":"hekla_location_2","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"ogc_fid":"1","cat":"1"},"geometry":{"type":"Point","coordinates":[-19.673672233472672,63.990923576356124]}}]}
